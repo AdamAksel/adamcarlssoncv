39d04fe0f22c78781ce2abfeaf7ae90fab15e55a
AdamAksel <adamcarlssoncoding@gmail.com>
2022-11-05 13:11:08 +0100

    first skeleton

---
+++ src/globalstyles.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    box-sizing:border-box;
+    margin:0;
+    padding:0;  
+
+    ::-webkit-scrollbar {
+    width: 5px;
+  }
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px gray; 
+    border-radius: 10px;
+    
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #141c3a; 
+    border-radius: 10px;
+    
+  }
+
+}
+
+body {
+        background-color: white;
+        color: #141c3a;
+    }
+
+
+`
+
+export default GlobalStyle
